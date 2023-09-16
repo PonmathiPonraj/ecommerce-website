9c90eb1dc0c8927900a11a3cc3762e01f0d52d2b
ponmathi <ponmathittl@gmail.com>
2023-09-16 12:27:37 +0530

    Cart Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-import Resourse from './Components/Resource';
+import React, { useState } from 'react';
+import CartModal from './Components/UI/CartModal';
+import Resource from './Components/Resource';
 
 function App() {
+  const initialCart = [
+    {
+      title: 'Colors',
+      price: 100,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+      quantity: 2,
+    },
+
+    {
+      title: 'Black and white Colors',
+      price: 50,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+      quantity: 3,
+    },
+
+    {
+      title: 'Yellow and Black Colors',
+      price: 70,
+      imageUrl:
+        'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+      quantity: 1,
+    },
+
+  ];
+
+  const [cartItems, setCartItems] = useState(initialCart);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const handleRemoveItem = (title) => {
+    const updatedCart = cartItems.filter((item) => item.title !== title);
+    setCartItems(updatedCart);
+  };
+
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
   return (
-    <div className="App">
+    <div>
+      <button onClick={toggleCart}>
+        Cart
+      </button>
+      {isCartOpen && (
+        <CartModal
+          cartItems={cartItems}
+          onRemoveItem={handleRemoveItem}
+          onClose={toggleCart}
+        />
+      )}
       <Resource />
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/Resource.js
+++ src/Components/Resource.js
@@ -1,41 +1,53 @@
+import React from 'react';
+import classes from './Resource.module.css';
+
 const productsArr = [
     {
         title: 'Colors',
-    price: 100,
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+        price: 100,
+        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
     },
 
     {
-    title: 'Black and white Colors',
-    price: 50,
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+        title: 'Black and white Colors',
+        price: 50,
+        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
     },
 
     {
-    title: 'Yellow and Black Colors',
-    price: 70,
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+        title: 'Yellow and Black Colors',
+        price: 70,
+        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
     },
 
     {
-    title: 'Blue Color',
-    price: 100,
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+        title: 'Blue Color',
+        price: 100,
+        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
     }
 ]
 
-const Resource=()=>{
+const Resource = () => {
+    const columns = [];
+    for (let i = 0; i < productsArr.length; i += 2) {
+        columns.push(productsArr.slice(i, i + 2));
+    }
 
-    const elements = 
-    <ul>
-        {
-            productsArr.map((ele)=>
-                title=ele.title,
-                price=ele.price,
-                imageUrl=ele.imageUrl
-            )
-        }
-    </ul>
+    const elements = (
+        <div className={classes['product-columns']}>
+            {columns.map((column, columnIndex) => (
+                <ul key={columnIndex} className={classes['product-column']}>
+                    {column.map((product) => (
+                        <li key={product.title}>
+                            <h2>{product.title}</h2>
+                            <p>Price: ${product.price}</p>
+                            <img src={product.imageUrl} alt={product.title} />
+                        </li>
+                    ))}
+                 </ul>
+            ))}
+        </div>
+    );
 
     return (
         <div>{elements}</div>
