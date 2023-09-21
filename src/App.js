4b7410f2fa60706bb416431005976d0fcacd85c2
ponmathi <ponmathittl@gmail.com>
2023-09-21 18:26:16 +0530

    Adding Login page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StorePage from './Pages/StorePage';
 import AboutPage from './Pages/AboutPage';
 import ContactPage from './Pages/ContactPage';
 import ProductDetail from './Product/ProductDetail';
+import LoginPage from './Pages/LoginPage';
 
 const App = () => {
   return (
@@ -13,9 +14,11 @@ const App = () => {
       <div>
         <MainHeader />
         <main>
-          <Route path="/home" component={HomePage} />
+          <Route path='/' exact><HomePage/></Route>
+          <Route path="/home" component={HomePage} exact />
           <Route path="/store" component={StorePage} />
           <Route path="/about" component={AboutPage} />
+          <Route path="/login"><LoginPage/></Route>
           <Route path="/contact" component={ContactPage} />
           <Route path="/product/:productId" component={ProductDetail} /> {/* Dynamic route */}
         </main>
