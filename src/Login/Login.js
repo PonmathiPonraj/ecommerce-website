dc3771587408c1d81c2fa9a5364f1ff8d818fdc8
ponmathi <ponmathittl@gmail.com>
2023-09-21 19:02:11 +0530

    final code

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,62 +1,71 @@
-import { useState, useRef } from "react";
+import { useState, useRef,useContext } from "react";
 import classes from "./Login.module.css";
+import AuthContext from "../AuthContext/AuthContext";
 import { useHistory } from "react-router-dom";
 
 const Login = () => {
-    const [isLogin, setLogin] = useState(true);
-    const [isLoading, setIsLoading] = useState(false);
-
     const emailInputRef = useRef();
     const passwordInputRef = useRef();
-
-    const history = useHistory();
-
+  
+    const authCtx=useContext(AuthContext);
+  
+    const  history=useHistory();
+  
+    const [isLoading,setisLoading]=useState(false);
+    const [isLogin, setIsLogin] = useState(true);
+  
     const switchAuthHandler = () => {
-        setLogin((prevState) => !prevState);
+        setIsLogin((prevState) => !prevState);
     };
 
     const submitHandler = (event) => {
         event.preventDefault();
+  
         const enteredEmail = emailInputRef.current.value;
         const enteredPassword = passwordInputRef.current.value;
-        setIsLoading(true);
-
-        if (isLogin) {
-            console.log(enteredEmail, enteredPassword);
-            history.push("/store");
-        } 
-        else {
-            fetch(
-                "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBe59Ffpjr8AlNv8oSvdCXFsjXtfB8EPpQ",
-                {
-                    method: "POST",
-                    body: JSON.stringify({
-                        email: enteredEmail,
-                        password: enteredPassword,
-                        returnSecureToken: true,
-                    }),
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            ).then((res) => {
-                setIsLoading(false);
-
-                console.log(res);
-                if (res.ok) {
-                    history.push("/store");
-                }
-                else {
-                    return res.json().then((data) => {
-                        let errorMessage = "Authentication fail";
-                        if (data && data.error && data.error.message) {
-                            errorMessage = data.error.message;
-                        }
-                        alert(errorMessage);
-                    });
-                }
-            });
+  
+        let url;
+  
+        setisLoading(true);
+  
+        if(isLogin){
+            url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAewrRSvuTkoFmEg6jPyX27gOkmIIAGL38'     
         }
+        else{
+            url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAewrRSvuTkoFmEg6jPyX27gOkmIIAGL38'
+        }
+        fetch(url,{
+            method:'POST',
+            body:JSON.stringify({
+                email:enteredEmail,
+                password:enteredPassword,
+                returnSecureToken:true,
+            }),
+            headers:{
+                'Content-Type':'application/json',
+            }
+        }
+        ).then((res)=>{
+            setisLoading(false);
+            if(res.ok){
+                return res.json();
+            }
+            else{
+                return res.json().then(data => {
+                    let errorMessage='Authentication Error';
+                    if(data && data.error && data.error.message){
+                        errorMessage=data.error.message;
+                    }
+                    throw new Error(errorMessage);
+                })
+            }
+        }).then((data)=>{
+            console.log(data);
+            authCtx.login(data.idToken);
+            history.push('/store')
+        }).catch((error)=>{
+            alert(error.message);
+        });
     };
 
     return (
@@ -68,21 +77,17 @@ const Login = () => {
                     <input type="email" id="email" ref={emailInputRef} required />
                 </div>
                 <div className={classes.control}>
-                    <label htmlFor="password">Enter Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        ref={passwordInputRef}
-                        required
-                    />
+                <label htmlFor="password">Enter Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    ref={passwordInputRef}
+                    required
+                />
                 </div>
                 <div className={classes.action}>
-                    {!isLoading && (
-                        <button type="submit">
-                            {isLogin ? "Login" : "Create Account"}
-                        </button>
-                    )}
-                    {isLoading && <p>Sending Request....</p>}
+                    {!isLoading && <button type="submit">{isLogin ? "Login" : "Create Account"}</button>}
+                    {isLoading && <p>Sending Your Request...</p>}
                     <button
                         type="button"
                         className={classes.toggle}
--- src/Pages/StorePage.js
+++ src/Pages/StorePage.js
@@ -1,9 +1,11 @@
+//import {useHistory} from  'react-router-dom'
 import Product from  "../Product/Product";
+//import { useEffect } from 'react';
 
 const StorePage = () => {
     return (
         <Product/>
     );
-};
+}; 
 
 export default StorePage;
