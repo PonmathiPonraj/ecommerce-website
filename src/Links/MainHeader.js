070b4607cf0204df732483985ec41f0367578db4
ponmathi <ponmathittl@gmail.com>
2023-09-21 17:19:30 +0530

    contact page

---
+++ src/Links/MainHeader.js
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+import classes from "./MainHeader.module.css";
+
+const MainHeader = () => {
+    return (
+        <header className={classes.header}>
+            <nav>
+                <ul className={classes.list}>
+                    <li>
+                        <NavLink to="/home">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/store">Store</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about">About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact">Contact Us</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default MainHeader;
